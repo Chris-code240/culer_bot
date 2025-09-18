0605763f898a6c994cd2ebe3372478fb61ed85bf
Chris-code240 <duah.marfochristian@gmail.com>
2025-09-18 07:58:59 +0000

    retriever: switch to sofascore api

--- retriever/apiclient.py
+++ retriever/apiclient.py
@@ -8,7 +8,7 @@ import http.client
 
 
 class APIClient:
-    def __init__(self, api_key:str = os.environ.get('FOOTBALL_API_KEY', None), base_url:str=os.environ.get('FOOTBALL_API_BASE_URL', None)):
+    def __init__(self, api_key:str = os.environ.get('x-rapidapi-key', None), base_url:str=os.environ.get('x-rapidapi-host', None)):
         if not api_key:
             raise ValueError("API_KEY cannot be None")
         if not base_url:
@@ -34,26 +34,66 @@ class APIClient:
             raise Exception(res.reason)
         except Exception as e:
             return {"success":False, "message":str(e)}
-    def get_player_by_name(self, player_name=None):
+    def get_player_details(self, playerID:int):
         try:
-            self.httpConnection.request("GET", f"https://{self.base_url.strip()}/players/profiles?search={player_name}".replace(' ', '%20'), headers=self.headers)
-            res = self.httpConnection.getresponse()
-            if res.status >= 200 and res.status < 300:
-                print(res.read().decode("utf-8"))
-                return { "success":True, "data":json.loads(res.read().decode("utf-8"))['response'][0]['player']}
-            raise Exception(res.reason)
+            url = f"http://{self.base_url}/players/detail"
+            response = requests.get(url=url, headers=self.headers, params={"playerId":str(playerID)})
+            if response.ok:
+                return response.json()
+            raise Exception(response.reason)
+        except Exception as e:
+            return  { "success":False, "message":str(e)}
+    def get_player_statistics(self, playerID:int):
+        try:
+            url = f"https://{self.base_url}/players/get-all-statistics"
+            params = {"playerId":str(playerID)}
+            response = requests.get(url=url, headers=self.headers, params=params)
+            if response.ok:
+                return response.json()
+            raise Exception(response.reason)
+        except Exception as e:
+            return { "success":False, "message":str(e)}
+    def get_player_by_name(self, player_name:str):
+        try:
+            response = requests.get(url = "https://"+self.base_url + "/search?", headers=self.headers, params={"q":player_name,"type":"all","page":0})
+            if response.ok:
+                return response.json()
+            raise Exception(response.reason)
         except Exception as e:
             return { "success":False, "message":str(e)}
     
     def get_squad(self):
         try:
-            res = requests.get(url = f"https://v3.football.api-sports.io/players/squads?team={self.barca_id}", headers=self.headers)
-            if res.ok:
-                return { "success":True, "data":res.json()['response'][0]['players']}
-            raise Exception(res.reason)
+            response = requests.get(url = "https://"+self.base_url + "/teams/squad", headers=self.headers, params={"teamId":str(self.barca_id)})
+            if response.ok:
+                return response.json()
+            raise Exception(response.reason)
         except Exception as e:
             return { "success":False, "message":str(e)}
-    
+    def get_player_characteristics(self, playerID:int):
+        try:
+            response = requests.get(url = "https://"+self.base_url + "/players/get-characteristics", headers=self.headers, params={"playerId":str(playerID)})
+            if response.ok:
+                return response.json()
+            raise Exception(response.reason)
+        except Exception as e:
+            return { "success":False, "message":str(e)}
+    def get_transfer_history(self, playerID:int):
+        try:
+            response = requests.get(url = "https://"+self.base_url + "/players/get-transfer-history", headers=self.headers, params={"playerId":str(playerID)})
+            if response.ok:
+                return response.json()
+            raise Exception(response.reason)
+        except Exception as e:
+            return { "success":False, "message":str(e)}
+    def get_player_characteristics(self, playerID:int):
+        try:
+            response = requests.get(url = "https://"+self.base_url + "/players/get-national-team-statistics", headers=self.headers, params={"playerId":str(playerID)})
+            if response.ok:
+                return response.json()
+            raise Exception(response.reason)
+        except Exception as e:
+            return { "success":False, "message":str(e)}
     def get_all_players(self):
         pass
     def get_players_in_a_season(self, season:str = "2024/25"):
@@ -68,44 +108,8 @@ class APIClient:
 
 client = APIClient()
 
-print(client.get_squad())
+print(client.get_player_by_name("lionel messi"))
 
 """
-{
-get: "teams"
-parameters: {   name: "barcelona"}
-errors: []
-results: 1
-paging: {
-
-    current: 1
-    total: 1
-
-}
-response: [{team: {
-
-    id: 529
-    name: "Barcelona"
-    code: "BAR"
-    country: "Spain"
-    founded: 1899
-    national: false
-    logo: "https://media.api-sports.io/football/teams/529.png"
-
-}
-venue: {
-
-                id: 19939
-                name: "Estadi Olímpic Lluís Companys"
-                address: "Carrer de l'Estadi"
-                city: "Barcelona"
-                capacity: 55926
-                surface: "grass"
-                image: "https://media.api-sports.io/football/venues/19939.png"
-            }
-        }
-    ]
-
-}
+ {'results': [{'entity': {'id': 12994, 'name': 'Lionel Messi', 'slug': 'lionel-messi', 'retired': False, 'userCount': 1559095, 'team': {'id': 337602, 'name': 'Inter Miami CF', 'nameCode': 'IMC', 'slug': 'inter-miami-cf', 'national': False, 'sport': {'id': 1, 'slug': 'football', 'name': 'Football'}, 'userCount': 1510605, 'teamColors': {'primary': '#212322', 'secondary': '#f6b5cc', 'text': '#f6b5cc'}, 'gender': 'M', 'fieldTranslations': {'nameTranslation': {'ar': 'إنتر ميامي', 'ru': 'Интер Майами', 'hi': 'इंटर मिआमि सीऍफ़'}, 'shortNameTranslation': {}}}, 'deceased': False, 'country': {'alpha2': 'AR', 'name': 'Argentina', 'slug': 'argentina'}, 'shortName': 'L. Messi', 'position': 'F', 'jerseyNumber': '10', 'sofascoreId': 'LM10', 'fieldTranslations': {'nameTranslation': {'ar': 'ل. ميسي', 'hi': 'एल. मेसी', 'bn': 'এল. মেসি'}, 'shortNameTranslation': {}}}, 'score': 1127260.6, 'type': 'player'}
 """
-
